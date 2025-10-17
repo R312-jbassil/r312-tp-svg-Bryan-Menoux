a7556b6e0aab97eacd064b2c6c9a49eb63085afd
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-17 09:12:51 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+<<<<<<< Updated upstream
 import node from "@astrojs/node";
+=======
+import netlify from "@astrojs/netlify";
+>>>>>>> Stashed changes
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +16,12 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+<<<<<<< Updated upstream
   adapter: node({
     mode: "standalone",
   }),
+=======
+
+  adapter: netlify(),
+>>>>>>> Stashed changes
 });
