14192014f0f6fd50a5e46171fe6a03508de40ef8
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-17 17:11:36 +0200

    modifs du code

--- src/pages/apis/logout.js
+++ src/pages/apis/logout.js
@@ -1,7 +1,10 @@
 export const POST = async ({ cookies }) => {
-  cookies.delete("pb_auth", {
+  // On réécrit le cookie pour le "vider" côté navigateur
+  cookies.set("pb_auth", "", {
     path: "/",
+    expires: new Date(0), // expire immédiatement
     httpOnly: true,
+    secure: true,
     sameSite: "strict",
   });
 
@@ -11,7 +14,7 @@ export const POST = async ({ cookies }) => {
   });
 };
 
-// Optionnel : GET visible pour tester
+// Optionnel : GET visible pour test
 export const GET = async () =>
   new Response(JSON.stringify({ message: "Logout endpoint ready" }), {
     status: 200,
