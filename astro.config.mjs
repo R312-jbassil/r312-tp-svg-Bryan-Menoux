02b0a10f790b959a7dd724defa8f5ea2ab623789
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-17 13:24:44 +0200

    modifs du code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 import node from "@astrojs/node";
