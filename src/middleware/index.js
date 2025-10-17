77ac8b42d1e376ed758aabdfcfef26dab0d278f0
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-17 16:47:08 +0200

    modifs du code

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -12,8 +12,8 @@ export const onRequest = async (context, next) => {
   }
 
   // --- ROUTES API : AUTH OBLIGATOIRE SAUF LOGIN / SIGNUP ---
-  if (context.url.pathname.startsWith("/api/")) {
-    const publicApiRoutes = ["/api/login", "/api/signup"];
+  if (context.url.pathname.startsWith("/apis/")) {
+    const publicApiRoutes = ["/apis/login", "/apis/signup"];
 
     // Tolère les variantes avec ou sans slash final
     const isPublicApi = publicApiRoutes.some(
