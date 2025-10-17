179fdf442644bf770bd7cd59bc00a79e7532fab2
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-17 17:40:13 +0200

    modifs du code

--- src/pages/apis/logout.js
+++ src/pages/apis/logout.js
@@ -1,14 +1,12 @@
-// /src/pages/apis/logout.js
-const clearCookie = (url) => {
-  const domain = new URL(url).hostname;
-  return new Response(null, {
+const clearCookie = () =>
+  new Response(null, {
     status: 303,
     headers: {
-      "Set-Cookie": `pb_auth=; Path=/; Domain=${domain}; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict`,
+      "Set-Cookie":
+        "pb_auth=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict",
       Location: "/",
     },
   });
-};
 
-export const GET = ({ request }) => clearCookie(request.url);
-export const POST = ({ request }) => clearCookie(request.url);
+export const POST = async () => clearCookie();
+export const GET = async () => clearCookie();
